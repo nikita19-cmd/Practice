c630253eae713afed77748b19ab30edca3670ccd
greeneyedcoder <jaiswalmahee22@gmail.com>
2021-05-01 23:16:06 +0530

    Update lists_tuples_sets.py

--- lists_tuples_sets.py
+++ lists_tuples_sets.py
@@ -15,7 +15,18 @@ append()	      Adds an element at the end of the list
                 print(List)
                 Output: ['Mathematics', 'chemistry', 1997, 2000, 20544]
 
-clear()	        Removes all the elements from the list
+clear()	        Removes all the elements from the list.
+                list = ['1','2','3']  
+                for l in list:  # Iterating list  
+                print(l)  
+                list.clear()  
+                print("After clearing:")  
+                for l in list:  # Iterating list  
+                print(l)  
+                Output:1
+                      2
+                      3
+                After clearing:
 
 max():          Calculates maximum of all the elements of List.
                 List = [2.3, 4.445, 3, 5.33, 1.054, 2.5]
@@ -33,7 +44,16 @@ min() :         Calculates minimum of all the elements of List.
                 Output:1.054          
 
 copy()	        Returns a copy of the list
-
+                lis1 = [ 1, 2, 3, 4 ]
+                lis2 = lis1.copy()
+                print ("The new list created is : " + str(lis2))
+                lis2.append(5)
+                print ("The new list after adding new element : " + str(lis2))
+                print ("The old list after adding new element to new list  : " + str(lis1))
+                Output: The new list created is : [1, 2, 3, 4]
+                        The new list after adding new element : [1, 2, 3, 4, 5]
+                        The old list after adding new element to new list  : [1, 2, 3, 4]
+                          
 count()	        Returns the number of elements with the specified value
                 List = [1, 2, 3, 1, 2, 1, 2, 3, 2, 1]
                 print(List.count(1))
@@ -48,7 +68,10 @@ extend()	      Add the elements of a list (or any iterable), to the end of the c
                         [2, 3, 4, 5, 1, 2, 3, 2, 3, 4, 5]
   
  
-index()	        Returns the index of the first element with the specified value
+index()	        Returns the index of the first element with the specified value.
+                List = [1, 2, 3, 1, 2, 1, 2, 3, 2, 1]
+                print(List.index(2))
+                Output:1
 
 insert()	      Adds an element at the specified position
                 List = ['Mathematics', 'chemistry', 1997, 2000]
@@ -73,7 +96,8 @@ remove()	      Removes the item with the specified value.
                 print(List)
                 Output: [2.3, 4.445, 5.33, 1.054, 2.5]
           
-reverse()      	Reverses the order of the list. Sort the given data structure (both tuple and list) in ascending order. Key and reverse_flag are not necessary parameter and reverse_flag is set to False, if nothing is passed through sorted().
+reverse()      	Reverses the order of the list. Sort the given data structure (both tuple and list) in ascending order. Key and reverse_flag are not necessary parameter 
+                and reverse_flag is set to False, if nothing is passed through sorted().
                 List = [2.3, 4.445, 3, 5.33, 1.054, 2.5]
                 #Reverse flag is set True
                 List.sort(reverse=True) 
@@ -81,7 +105,14 @@ reverse()      	Reverses the order of the list. Sort the given data structure (b
                 print(List)        
                 Output: [5.33, 4.445, 3, 2.5, 2.3, 1.054]
               
-sort()	        Sorts the list
+sort()	        Sorts the list in particular manner.
+                # vowels list
+                vowels = ['e', 'a', 'u', 'o', 'i']
+                # sort the vowels
+                vowels.sort()
+                # print vowels
+                print('Sorted list:', vowels)
+                Output: Sorted list: ['a', 'e', 'i', 'o', 'u']
 
 sum() :         Calculates sum of all the elements of List.
                 List = [1, 2, 3, 4, 5]
