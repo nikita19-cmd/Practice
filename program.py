4a9a263c084f91fb6b2deeb2569d7c2dc900b662
greeneyedcoder <jaiswalmahee22@gmail.com>
2021-05-02 14:44:31 +0530

    Update program.py

--- program.py
+++ program.py
@@ -2,9 +2,9 @@
 
 number = float(input("Enter a number: "))
 
-if number > 0:
+```if number > 0:
     print("The number is positive")
 elif number < 0:
     print("The number is negative")
 else:
-    print("The number is 0")
+    print("The number is 0")```
